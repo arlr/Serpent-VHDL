cd520aa65e6c7bd9c437f93ffb65f58454309731
Arthur <arlrcode@gmail.com>
2020-03-18 11:30:39 +0100

    Test et validation de KeyExpansion

---
+++ Serpent-VHDL.srcs/sim_1/new/TB_KeyExpansion.vhd
@@ -0,0 +1,153 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 17.03.2020 15:56:04
+-- Design Name: 
+-- Module Name: TB_KeyExpansion - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity TB_KeyExpansion is
+--  Port ( );
+end TB_KeyExpansion;
+
+architecture Behavioral of TB_KeyExpansion is
+
+component KeyExpansion is
+    Port ( 
+    -- IN
+    key : in std_logic_vector(255 downto 0);
+    clk : in std_logic;
+    rst : in std_logic;
+    i : in integer range 0 to 131;
+    -- OUT
+    wi : out std_logic_vector(31 downto 0)
+    );
+end component;
+
+signal clk : std_logic := '0';
+signal rst : std_logic := '0';
+signal i : integer range 0 to 131 := 0;
+
+signal key : std_logic_vector(255 downto 0);
+signal wi : std_logic_vector(31 downto 0);
+
+begin
+
+dut : KeyExpansion
+port map(
+    clk => clk,
+    rst => rst,
+    i => i,
+    key => key,
+    wi => wi
+    );
+    
+clk <= not clk after 5 ns;
+
+process begin
+
+rst <= '1';
+
+wait for 5 ns;
+
+rst <= '0';
+wait for 5 ns;
+
+
+key  <= "0100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+i <= 0;
+wait for 20 ns;
+
+i <= 1;
+wait for 20 ns;
+
+i <= 2;
+wait for 20 ns;
+
+i <= 3;
+wait for 20 ns;
+
+i <= 4;
+wait for 20 ns;
+
+i <= 5;
+wait for 20 ns;
+
+
+i <= 6;
+wait for 20 ns;
+
+
+i <= 7;
+wait for 20 ns;
+
+
+i <= 8;
+wait for 20 ns;
+
+
+i <= 9;
+wait for 20 ns;
+
+i <= 10;
+wait for 20 ns;
+
+i <= 11;
+wait for 20 ns;
+
+i <= 12;
+wait for 20 ns;
+
+i <= 13;
+wait for 20 ns;
+
+i <= 14;
+wait for 20 ns;
+
+i <= 15;
+wait for 20 ns;
+
+i <= 16;
+wait for 20 ns;
+
+i <= 17;
+wait for 20 ns;
+
+i <= 18;
+wait for 20 ns;
+
+i <= 19;
+wait for 20 ns;
+
+rst <= '1';
+wait for 20 ns;
+
+
+end process;
+
+end Behavioral;
--- Serpent-VHDL.srcs/sources_1/new/KeyExpansion.vhd
+++ Serpent-VHDL.srcs/sources_1/new/KeyExpansion.vhd
@@ -21,6 +21,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -37,7 +38,7 @@ Port (
     key : in std_logic_vector(255 downto 0);
     clk : in std_logic;
     rst : in std_logic;
-    i : in integer range 0 to 131;
+    i : in integer range 0 to 131 := 0;
     -- OUT
     wi : out std_logic_vector(31 downto 0)
     );
@@ -48,17 +49,19 @@ type fifo is array (8 downto 0) of std_logic_vector(31 downto 0);
 type state_t is (init, run);
 signal my_fifo : fifo;
 signal state : state_t := init;
+signal old_i : integer range -1 to 131 := 0;
 
 begin
 
 process(rst, clk) 
-    variable phi : bit_vector(31 downto 0) := x"9e3779b9" ;
-    variable old_i : integer range 0 to 131 := 0;
+    variable phi : std_logic_vector(31 downto 0) := x"9e3779b9" ;
 begin
 if rst = '1' then
     my_fifo <= (0 to 8 => (others =>'0')); -- Set to 0 important values
+    wi <= (others => '0');
+    old_i <= 0;
     state <= init;
-    old_i := 0;
+    
     
 elsif clk'event and clk = '1' then
     case state is
@@ -74,15 +77,16 @@ elsif clk'event and clk = '1' then
             state <= run;
         
         when run =>
-            my_fifo(8) <= to_stdlogicvector( (to_bitvector(my_fifo(7) xor my_fifo(4) xor my_fifo(2) xor my_fifo(0)) xor phi) rol 11 );    --Expansion
-            wi <= my_fifo(8);
             -- Keep only useful values
             if (i /= old_i) then
+                wi <= my_fifo(8);
                 for j in 0 to 7 loop
                     my_fifo(j) <= my_fifo(j + 1);
                 end loop;
-                old_i := i;
+                old_i <= i;
             end if;
+            my_fifo(8) <= to_stdlogicvector( to_bitvector( my_fifo(7) xor my_fifo(5) xor my_fifo(3) xor my_fifo(0)  xor phi xor std_logic_vector(to_unsigned(i, 32)) )  rol 11 );    --Expansion
+
             
     end case;
  
