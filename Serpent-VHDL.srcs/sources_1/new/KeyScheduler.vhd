63e65b8c6191edbe7eb4b97be7524639933fb859
Arthur <arlrcode@gmail.com>
2020-03-20 15:32:30 +0100

    Modification de Demu_SBUX pour la passer en asynchrone

--- Serpent-VHDL.srcs/sources_1/new/KeyScheduler.vhd
+++ Serpent-VHDL.srcs/sources_1/new/KeyScheduler.vhd
@@ -34,10 +34,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity KeyScheduler is
 Port ( 
     -- IN
-    clk : in std_logic;
-    rst : in std_logic;
     key : in std_logic_vector(255 downto 0);
-    addr_mem : in integer range 0 to 32;
+    i : in integer range 0 to 32;
     -- OUT
     Ki : out std_logic_vector(127 downto 0)
 );
@@ -45,15 +43,13 @@ end KeyScheduler;
 
 architecture Behavioral of KeyScheduler is
 
-component KeyExpansion
+component KeyExpansionAsync
 Port(
     -- IN
     key : in std_logic_vector(255 downto 0);
-    clk : in std_logic;
-    rst : in std_logic;
-    i : in integer range 0 to 131 := 0;
+    addr : in integer range 0 to 31 := 0;
     -- OUT
-    wi : out std_logic_vector(31 downto 0)
+    val_wi : out std_logic_vector(127 downto 0)
 );
 end component;
 
@@ -142,22 +138,8 @@ component Demu_S_Box
     data_in7 : in std_logic_vector(127 downto 0);
     
     SEL_SBOX : in integer range 0 to 7 ; -- 8 possibilités
-    clk : in std_logic;
     -- OUT
     data_out : out std_logic_vector(127 downto 0)
-    );
-end component;
-
-component memK 
-  Port (
-    -- IN
-    rst : in std_logic;
-    clk : in std_logic;
-    rw : in std_logic := '0';  -- 1  == Write
-    val_in : in std_logic_vector(127 downto 0);
-    addr : in integer range 0 to 32;
-    -- OUT
-    val_out : out std_logic_vector(127 downto 0)
     );
 end component;
 
@@ -165,13 +147,9 @@ end component;
 
 -- Définition du type mémoire
 type memoire_t is array (131 downto 0) of std_logic_vector(31 downto 0);
-type state_t is (KeyExpand, sbox, read);
-signal Wi_mem : memoire_t;
-signal state : state_t := KeyExpand;
 
 -- KeyExpand
-signal IN_EXP : integer range 0 to 131 := 0;
-signal OUT_EXP : std_logic_vector(31 downto 0);
+signal OUT_EXP : std_logic_vector(127 downto 0);
 -- SBOX
 signal vec_SBOX : std_logic_vector(127 downto 0);
 signal IN_SBOX : std_logic_vector(127 downto 0);
@@ -183,21 +161,18 @@ signal OUT_SBOX4 : std_logic_vector(127 downto 0);
 signal OUT_SBOX5 : std_logic_vector(127 downto 0);
 signal OUT_SBOX6 : std_logic_vector(127 downto 0);
 signal OUT_SBOX7 : std_logic_vector(127 downto 0);
-signal SELECT_SBOX : integer range 0 to 7 ; -- 8 possibilités
+signal SELECT_SBOX : integer range 0 to 7; -- 8 possibilités
 signal VAL_SBOX : std_logic_vector(127 downto 0);
--- memK
-signal rw_memK : std_logic;  -- 1  == Write
-signal addr_memK : integer range 0 to 32;
-signal val_in_memK : std_logic_vector(127 downto 0);
-signal val_out_memK : std_logic_vector(127 downto 0);
+
 
 
 begin
 
+SELECT_SBOX <= (3 - i) mod 8;
 
 -- ======================== PORT MAP ========================
 
-KeyExp : KeyExpansion port map(key => key, clk => clk, rst => rst, i => IN_EXP, wi => OUT_EXP );
+KeyExpAsync : KeyExpansionAsync port map(key => key, addr => i, val_wi => IN_SBOX);
 
 SBoxBig0 : Sbox_Big0 port map( data_in => IN_SBOX, data_out => OUT_SBOX0);
 SBoxBig1 : Sbox_Big1 port map( data_in => IN_SBOX, data_out => OUT_SBOX1);
@@ -207,6 +182,7 @@ SBoxBig4 : Sbox_Big4 port map( data_in => IN_SBOX, data_out => OUT_SBOX4);
 SBoxBig5 : Sbox_Big5 port map( data_in => IN_SBOX, data_out => OUT_SBOX5);
 SBoxBig6 : Sbox_Big6 port map( data_in => IN_SBOX, data_out => OUT_SBOX6);
 SBoxBig7 : Sbox_Big7 port map( data_in => IN_SBOX, data_out => OUT_SBOX7);
+
 Demu_SBox : Demu_S_Box port map( data_in0 => OUT_SBOX0,
                             data_in1 => OUT_SBOX1,
                             data_in2 => OUT_SBOX2, 
@@ -216,65 +192,8 @@ Demu_SBox : Demu_S_Box port map( data_in0 => OUT_SBOX0,
                             data_in6 => OUT_SBOX6, 
                             data_in7 => OUT_SBOX7, 
                             SEL_SBOX => SELECT_SBOX, 
-                            clk => clk, 
-                            data_out => VAL_SBOX );
-memoire_K : memK port map (rst => rst, clk => clk, rw => rw_memk, addr=> addr_memk, val_in => val_in_memK, val_out => val_out_memk);
+                            data_out => Ki );
 
-process(rst, clk) 
-
-variable ExpandOUT : std_logic_vector(127 downto 0);
-
-begin
-if rst = '1' then 
-    -- Met la mémoire à 0
-    Wi_mem <= (others => (others => '0'));
-    IN_EXP <= 0;
-    
-    state <= KeyExpand;
-    
-elsif clk'event and clk ='1' then
-    case state is
-        when KeyExpand =>
-            
-            for i in 0 to 131 loop
-                if i < 131 then
-                    IN_EXP <= i;
-                else
-                    IN_EXP <= 0;
-                end if;
-                
-                if (i > 0) then
-                -- Gestion du décallage entre les valeurs demandé et reçu par le KeyExpand
-                    Wi_mem(i-1) <= OUT_EXP;
-                end if;
-            end loop;
-            state <= SBOX;
-         
-         when sbox =>
-            rw_memk <= '1';
-            for K in 0 to 32 loop
-                    
-                    vec_SBOX(127 downto 96) <= Wi_mem(K*4+0);
-                    vec_SBOX(95 downto 64) <= Wi_mem(K*4+1);
-                    vec_SBOX(63 downto 32) <= Wi_mem(K*4+2);
-                    vec_SBOX(31 downto 0) <= Wi_mem(K*4+3);
-                    IN_SBOX <= vec_SBOX;
-                    SELECT_SBOX <= (3-K) mod 8;
-                    addr_memk <= K;
-                    val_in_memk <= VAL_SBOX;
-                    
-            end loop;
-            state <= read;
-         
-         when read =>
-           rw_memk <= '0';
-           Ki <= val_out_memK;
-
-
-    end case;
-
-end if;
-end process;
 
 
 end Behavioral;
